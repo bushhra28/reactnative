756bec32f4bd09432c3ee5bf0bf546575b95c97d
bushhra28 <bushra.maqsoodbaig@gmail.com>
2021-12-30 14:19:34 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text style= {styles.textcolour1}>
+        Bushra
+      </Text>
+      <Text style={styles.textcolour2}>
+        Bushra2
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent: "space-around",
+    
+  },
+  textcolour1:{
+    backgroundColor: "pink"
+  },
+  textcolour2:{
+    backgroundColor:"blue"
+  }
+});
+
+export default App;
